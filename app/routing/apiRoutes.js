37fbb96282007de97e1c962c0463ddf4d0fe2c95
jeanshotjack <dshalenkova@gmail.com>
2019-11-21 20:53:28 -0500

    absolute value math (hopefully)

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,10 +16,12 @@ app.get("/api/friends", function(req, res) {
     //these get called in survey.html
     var matchName = "";
     var matchPhoto = "";
+    var totalMath = 40; //5 possible responses, 10 questions, 5-1 = 4, 4 * 10 = 40 , 40 is the largest difference someone can have 
 
     for (i=0; i<friends.length; i++) {
+        var math = 0;
         for (j=0; j<urScore.length; j++){
-            // absolute vslue here I think
+            math += Math.abs(friends[i].scores[j] - urScore[j]);
         }
         // assign the match stuff to the empty variable????
     }
