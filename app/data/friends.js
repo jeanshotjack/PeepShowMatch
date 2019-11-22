29c41e32b4e29feec9b4f9ef3f54b28637b8b221
jeanshotjack <dshalenkova@gmail.com>
2019-11-22 11:39:39 -0500

    added scores for preset friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,52 +2,204 @@ var friends = [
     {
         name: "Mark",
         photo: "https://ksassets.timeincuk.net/wp/uploads/sites/55/2015/11/2015_PeepShow_Mark1_Press_111115-920x610.jpg",
-        scores: []
+        scores: [
+            1,
+            5,
+            5,
+            5,
+            5,
+            5,
+            1,
+            1,
+            1,
+            5,
+            5,
+            1,
+            5,
+            1
+        ]
 
     },
     {
         name: "Jeremy",
         photo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/11/10/15/jez-worst-turkey.jpg",
-        scores: []
+        scores: [
+            5,
+            1,
+            1,
+            1,
+            1,
+            1,
+            5,
+            5,
+            5,
+            1,
+            1,
+            5,
+            1,
+            5
+        ]
     },
     {
         name: "Super Hans",
         photo: "https://i.redd.it/neu4ogrqm3uy.jpg",
-        scores: []
+        scores: [
+            5,
+            2,
+            3,
+            4,
+            4,
+            2,
+            4,
+            5,
+            4,
+            1,
+            5,
+            1,
+            2,
+            4
+        ]
     },
     {
         name: "Sophie",
         photo: "https://amp.insider.com/images/5c742f72eb3ce84fb63638d5-750-563.jpg",
-        scores: []
+        scores: [
+            2,
+            4,
+            4,
+            2,
+            3,
+            4,
+            1,
+            2,
+            2,
+            5,
+            1,
+            5,
+            4,
+            1
+        ]
     },
     {
         name: "Dobby",
         photo: "https://i.ytimg.com/vi/CZUNBPlGt-k/maxresdefault.jpg",
-        scores: []
+        scores: [
+            4,
+            2,
+            2,
+            2,
+            2,
+            2,
+            4,
+            4,
+            4,
+            2,
+            2,
+            4,
+            2,
+            4
+        ]
     },
     {
         name: "Johnson",
         photo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/11/10/15/Johnson.jpg",
-        scores: []
+        scores: [
+            4,
+            1,
+            2,
+            5,
+            4,
+            5,
+            1,
+            5,
+            4,
+            2,
+            4,
+            2,
+            5,
+            1
+        ]
     },
     {
         name: "Big Suze",
         photo: "https://httpsak-a.akamaihd.net/2540076170001/2540076170001_5666150832001_5666148301001-vs.jpg?pubId=2540076170001&videoId=5666148301001",
-        scores: []
+        scores: [
+            4,
+            5,
+            4,
+            1,
+            1,
+            5,
+            2,
+            4,
+            1,
+            5,
+            3,
+            5,
+            4,
+            2
+        ]
     },
     {
         name: "Gerard",
         photo: "https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/42/2016/03/Bv_O4FGCQAE3emt-640x359.jpg",
-        scores: []
+        scores: [
+            1,
+            2,
+            2,
+            2,
+            2,
+            5,
+            1,
+            1,
+            5,
+            2,
+            3,
+            4,
+            4,
+            2
+        ]
     },
     {
         name: "Jeff",
         photo: "https://i2.lisimg.com/9218822/411full.jpg",
-        scores: []
+        scores: [
+            5,
+            5,
+            5,
+            4,
+            3,
+            1,
+            5,
+            4,
+            2,
+            4,
+            4,
+            2,
+            1,
+            4
+        ]
     },
     {
         name: "Nancy",
         photo: "https://vignette.wikia.nocookie.net/peepshow/images/6/62/Nancy.jpg/revision/latest?cb=20090619223410",
-        scores: []
+        scores: [
+            4,
+            3,
+            3,
+            3,
+            2,
+            3,
+            4,
+            4,
+            5,
+            2,
+            1,
+            5,
+            2,
+            4
+        ]
     }
 ];
+
+module.exports = friends;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -12,4 +12,8 @@ module.exports = function (app) {
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
+
+    app.get("/api/friends", function (req, res) {
+        res.sendFile(path.join(__dirname, "../data/friends.js"));
+    });
 }
