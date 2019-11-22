515b94b829a18901b2c467aad9aa2209125d24b3
jeanshotjack <dshalenkova@gmail.com>
2019-11-21 20:41:54 -0500

    not really sure if this is correct but I need the variables for the math I'm about to do lol

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,6 +10,13 @@ app.get("/api/friends", function(req, res) {
     var newFriend = req.body;
     console.log("req.body:");
     console.log(req.body);
+
+    var urScore = newFriend.scores;
+
+    //these get called in survey.html
+    var matchName = "";
+    var matchPhoto = "";
+
     friends.push(newFriend);
     res.json(newFriend);
   });
