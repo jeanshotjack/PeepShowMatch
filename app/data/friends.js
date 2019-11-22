946b530cf2aba9832e450434d602f786a4d4724d
jeanshotjack <dshalenkova@gmail.com>
2019-11-21 23:04:59 -0500

    added images to friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,28 +1,53 @@
 var friends = [
     {
-      name: "Mark",
-      photo: "",
-      scores: []
-  
+        name: "Mark",
+        photo: "https://ksassets.timeincuk.net/wp/uploads/sites/55/2015/11/2015_PeepShow_Mark1_Press_111115-920x610.jpg",
+        scores: []
+
     },
     {
-      name: "Jeremy",
-      photo: "",
-      scores: []
+        name: "Jeremy",
+        photo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/11/10/15/jez-worst-turkey.jpg",
+        scores: []
     },
     {
-      name: "Super Hans",
-      photo: "",
-      scores: []
+        name: "Super Hans",
+        photo: "https://i.redd.it/neu4ogrqm3uy.jpg",
+        scores: []
     },
     {
-      name: "Sophie",
-      photo: "",
-      scores: []
+        name: "Sophie",
+        photo: "https://amp.insider.com/images/5c742f72eb3ce84fb63638d5-750-563.jpg",
+        scores: []
     },
     {
-      name: "Dobby",
-      photo: "",
-      scores: []
+        name: "Dobby",
+        photo: "https://i.ytimg.com/vi/CZUNBPlGt-k/maxresdefault.jpg",
+        scores: []
+    },
+    {
+        name: "Johnson",
+        photo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/11/10/15/Johnson.jpg",
+        scores: []
+    },
+    {
+        name: "Big Suze",
+        photo: "https://httpsak-a.akamaihd.net/2540076170001/2540076170001_5666150832001_5666148301001-vs.jpg?pubId=2540076170001&videoId=5666148301001",
+        scores: []
+    },
+    {
+        name: "Gerard",
+        photo: "https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/42/2016/03/Bv_O4FGCQAE3emt-640x359.jpg",
+        scores: []
+    },
+    {
+        name: "Jeff",
+        photo: "https://i2.lisimg.com/9218822/411full.jpg",
+        scores: []
+    },
+    {
+        name: "Nancy",
+        photo: "https://vignette.wikia.nocookie.net/peepshow/images/6/62/Nancy.jpg/revision/latest?cb=20090619223410",
+        scores: []
     }
-  ];
+];
