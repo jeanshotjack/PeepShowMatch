6e88f9d69e399d35badbd62dcc8e8fb3b0793ac3
jeanshotjack <dshalenkova@gmail.com>
2019-11-21 22:11:48 -0500

    we areeeee onnnnnlineeeee! forgot to add module exports

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,36 +5,38 @@ var app = express();
 
 var friends = require("../data/friends.js");
 
-app.get("/api/friends", function(req, res) {
-    return res.json(friends);
-  });
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        return res.json(friends);
+    });
 
-  app.post("/api/friends", function(req, res) {
-    var newFriend = req.body;
-    console.log("req.body:");
-    console.log(req.body);
+    app.post("/api/friends", function (req, res) {
+        var newFriend = req.body;
+        console.log("req.body:");
+        console.log(req.body);
 
-    var urScore = newFriend.scores;
+        var urScore = newFriend.scores;
 
-    //these get called in survey.html
-    var matchName = "";
-    var matchPhoto = "";
-    var totalMath = 40; //5 possible responses, 10 questions, 5-1 = 4, 4 * 10 = 40 , 40 is the largest difference someone can have 
+        //these get called in survey.html
+        var matchName = "";
+        var matchPhoto = "";
+        var totalMath = 40; //5 possible responses, 10 questions, 5-1 = 4, 4 * 10 = 40 , 40 is the largest difference someone can have 
 
-    for (i=0; i<friends.length; i++) {
-        var math = 0;
-        for (j=0; j<urScore.length; j++){
-            math += Math.abs(friends[i].scores[j] - urScore[j]);
+        for (i = 0; i < friends.length; i++) {
+            var math = 0;
+            for (j = 0; j < urScore.length; j++) {
+                math += Math.abs(friends[i].scores[j] - urScore[j]);
+            }
+            // assign the match stuff to the empty variable????
+            if (math < totalMath) {
+
+                totalMath = diff;
+                matchName = friends[i].name;
+                matchPhoto = friends[i].photo;
+            }
         }
-        // assign the match stuff to the empty variable????
-        if (math < totalMath) {
 
-            totalMath = diff;
-            matchName = friends[i].name;
-            matchPhoto = friends[i].photo;
-        }
-    }
-
-    friends.push(newFriend);
-    res.json(newFriend);
-  });
+        friends.push(newFriend);
+        res.json(newFriend);
+    });
+}
